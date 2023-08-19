45f0640d209785b40887ac01d99db6844907c099
Sylvia <110571069+sylvia45335@users.noreply.github.com>
2023-08-18 22:58:25 -0700

    setting up files

---
+++ client/App.jsx
@@ -0,0 +1,12 @@
+import * as _ from 'lodash';
+import * as React from "react";
+
+function App() {
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    )
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/client/index.js',
+  output: {
+    path: path.join(__dirname, '/chrome-extension/dist'),
+    publicPath: '/',
+    filename: 'dist.js',
+  },
+  devtool: 'inline-source-map',
+  mode: 'development',
+  devServer: {
+    host: 'localhost',
+    hot: true,
+    open: true,
+    compress: true,
+    // historyApiFallback: true,
+    port: 8080,
+    static: {
+      directory: path.join(__dirname, '/dist'),
+      publicPath: '/',
+    },
+    proxy: {
+      '/': 'http://localhost:3001',
+      secure: false,
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, './client/index.html'),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$|jsx/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+            },
+          },
+        ],
+      },
+      {
+        test: /.(css|scss)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|svg|jpe?g|gif)$/,
+        include: /images/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'images/',
+              publicPath: 'images/'
+            }
+          }
+        ]
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      react: path.resolve('./node_modules/react'),
+    },
+  },
+};

eed747b357f90d9ff285770a8cdc1b747244d89b
Sylvia <110571069+sylvia45335@users.noreply.github.com>
2023-08-19 19:31:51 -0700

    set up server page and is running

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+require('dotenv').config({path:__dirname+'./.env'});
 const PORT = process.env.PORT || 3002;
 const CLIENT_ID = process.env.CLIENT_ID;
 const SECRET_KEY = process.env.SECRET_KEY;
@@ -10,7 +10,40 @@ const REDIRECT_URI = process.env.REDIRECT_URI || `http://localhost:${PORT}/home`
 const SpotifyWebApi = require("spotify-web-api-node");
 const spotifyAuthAPI = new SpotifyWebApi({
     clientId: CLIENT_ID,
-    clientSecret: CLIENT_SECRET,
+    clientSecret: SECRET_KEY,
     redirectUri: REDIRECT_URI
 });
 
+//setting up server
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+//cookies
+const cookieParser = require("cookie-parser");
+
+//for pathing
+const path = require('path');
+
+//json parsing
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+//for cors header if making calls to backend api
+app.use(cors());
+
+//configure middleware and static pages here
+app.use(express.static(path.join(__dirname, '../client/static')));
+
+//routes
+app.get('/login', (req, res) => {});
+
+app.get('/home', (req, res) => {});
+
+const accTokenRefresh = (req, res, next) => {};
+
+app.get('/faves', (req, res) => {});
+
+console.log('authTest running on port' + PORT);
+app.listen(PORT);

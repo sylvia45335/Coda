d28cbaf506e0b39ec087c773c49e69feb3cdbe09
Sylvia <110571069+sylvia45335@users.noreply.github.com>
2023-08-20 16:05:03 -0700

    added login endpoint logic to redirect user to spotify account

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-require('dotenv').config({path:__dirname+'./.env'});
+require('dotenv').config();
 const PORT = process.env.PORT || 3002;
 const CLIENT_ID = process.env.CLIENT_ID;
 const SECRET_KEY = process.env.SECRET_KEY;
@@ -34,10 +34,42 @@ app.use(cookieParser());
 app.use(cors());
 
 //configure middleware and static pages here
-app.use(express.static(path.join(__dirname, '../client/static')));
+app.use(express.static(path.join(__dirname, '../client/public')));
 
 //routes
-app.get('/login', (req, res) => {});
+//leads the user to the Spotify page for the user to enter their login credentials
+app.get('/login', (req, res) => {
+    //recommended to adding a state variable to avoid cross site attacks
+    //generate a random string of letters and numbers
+    const generateRandomString = (length) => {
+        let text = "";
+        let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+        for (let i = 0; i < length; i++) {
+            //gets a random character in our possible string
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+        }
+        return text;
+    }
+
+    const stateString = generateRandomString(16);
+    //add random string to user's cookies and query string
+    res.cookie("authState", stateString);
+
+    //ask Spotify for an authorization code filled with a set of permissions called Scopes
+    //allows our application to legally make requests to Spotify's API with the user's allowance in mind
+
+    //user-top-read is for user's Top Tracks and Albums
+    const scopes = ["user-top-read"];
+    //spotify node api method that creates a link that leads to the Spotify Account Login page with a set of requested scopes
+    //second param is state and is optional
+        //state we pass will be returned from Spotify
+        //check against the user's cookies to make sure it matches, no match = boot them out
+    const loginLink = spotifyAuthAPI.createAuthorizeURL(scopes, stateString);
+
+    //send user to Spotify Account Login
+    res.redirect(loginLink);
+});
 
 app.get('/home', (req, res) => {});
 

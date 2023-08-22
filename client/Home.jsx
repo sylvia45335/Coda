6521642d9d0ad7db9814585553b3d184e488ff42
Sylvia <110571069+sylvia45335@users.noreply.github.com>
2023-08-22 15:39:00 -0700

    Revert "fixed infinite loop from useeffect hooks, made api calls for track component and artist component"
    This reverts commit 125e4fb6296cc1b87635964a3ed460335db2ace3.

--- client/Home.jsx
+++ client/Home.jsx
@@ -4,39 +4,11 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import Artists from './components/Artists.jsx';
 import Tracks from './components/Tracks.jsx';
-import Genres from './components/Genres.jsx';
-import myCookies from './cookiesfunc.js';
 
 function Home() {
-    const [user, setUser] = useState('');
-
-    const PORT = process.env.REACT_APP_PORT;
-    const apiURL = `http://localhost:${PORT}/api/profile`
-
-    useEffect(() => {
-        axios({
-            method: 'get',
-            url: apiURL,
-            withCredentials: false,
-            headers: {
-                Authorization: myCookies
-            },
-          }).then((res) => {
-            setUser(res.data['display_name']);
-          })
-    },[])
     return (
         <div>
-            Welcome, {user}!
-            <div>
-              <Artists />
-            </div>
-            <div>
-              <Tracks />
-            </div>
-            <div>
-              <Genres />
-            </div>
+            <Tracks />
         </div>
     )
 };
--- client/components/Artists.jsx
+++ client/components/Artists.jsx
@@ -1,38 +1,14 @@
 import * as _ from 'lodash';
 import * as React from "react";
 import axios from 'axios';
-import { useEffect, useState } from 'react';
-import myCookies from '../cookiesfunc.js';
 
 function Artists() {
-    const [artists, setArtists] = useState([]);
-
     const PORT = process.env.REACT_APP_PORT;
     const apiURL = `http://localhost:${PORT}/api/artists`
 
-
-    useEffect(() => {
-        axios({
-            method: 'get',
-            url: apiURL,
-            withCredentials: false,
-            headers: {
-                Authorization: myCookies
-            },
-          }).then((res) => {
-            // console.log(res.data);
-            const artList = [];
-
-            for(let i = 0; i < res.data.length; i++) {
-                artList.push(res.data[i].name);
-            }
-            
-            setArtists(artList);
-          }, [])
-    })
     return(
         <div>
-            {artists}
+            Artists
         </div>
     )
 }
